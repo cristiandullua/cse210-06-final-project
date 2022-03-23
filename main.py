0d0b1e701a24007002677fb877bf17cd564a512c
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-03-23 14:19:30 -0300

    Adding the main function and importing BuiltWindow,Mouse
    from game.built_window import BuiltWindow
    from game.mouse import Mouse
    from raylib.colors import (RAYWHITE,LIGHTGRAY,RED,DARKGRAY,MAROON,LIME,DARKBLUE)
    def main():
        BuiltWindow()
    if __name__=="__main__":
        main()

---
+++ main.py
@@ -0,0 +1,10 @@
+from game.built_window import BuiltWindow
+from game.mouse import Mouse
+from raylib.colors import (RAYWHITE,LIGHTGRAY,RED,DARKGRAY,MAROON,LIME,DARKBLUE)
+def main():
+
+    BuiltWindow()
+    
+
+if __name__=="__main__":
+    main()
