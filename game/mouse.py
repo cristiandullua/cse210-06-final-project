1264af653940b0d053a4bf1230f7b39322e4dce8
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-03-23 14:37:53 -0300

    Mouse Text/string modifyied
     print("Coordenadas del Mouse  ",ball_color)

--- game/mouse.py
+++ game/mouse.py
@@ -8,13 +8,13 @@ class Mouse():
 
         if pyray.is_mouse_button_pressed(pyray.MOUSE_BUTTON_LEFT):
             ball_color = MAROON
-            print("soyyy  ",ball_color)
-            print("soyyy  ",self.ball_position)
+            print("Coordenadas del Mouse  ",ball_color)
+            #print("soyyy  ",self.ball_position)
         elif pyray.is_mouse_button_pressed(pyray.MOUSE_BUTTON_MIDDLE):
             ball_color = LIME
-            print("soyyy  ",ball_color)
-            print("soyyy  ",self.ball_position)
+            print("Coordenadas del Mouse  ",ball_color)
+            #print("soyyy  ",self.ball_position)
         elif pyray.is_mouse_button_pressed(pyray.MOUSE_BUTTON_RIGHT):
             ball_color = DARKBLUE
-            print("soyyy  ",ball_color)
-            print("soyyy  ",self.ball_position)
+            print("Coordenadas del Mouse  ",ball_color)
+            #print("soyyy  ",self.ball_position)
