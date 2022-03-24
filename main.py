8c7f77e372d36bb6e6df2b3fe4ba79bfb5bcbfa2
Cristian D Ullua <cris.ullua.95@gmail.com>
2022-03-24 16:35:24 -0300

    setting the bases

--- main.py
+++
@@ -1,9 +0,0 @@
-from game.built_window import BuiltWindow
-from raylib.colors import (RAYWHITE,LIGHTGRAY,RED,DARKGRAY,MAROON,LIME,DARKBLUE)
-def main():
-
-    BuiltWindow()
-    
-
-if __name__=="__main__":
-    main()
